2f22255c9e0f1cacb21933a5ed09c7555b445849
itop <itop@mail.datagearbi.com>
2020-04-11 22:15:14 +0200

    modified:   make_prediction.sh 	modified:   run_docker.sh 	modified:   run_kubernetes.sh 	modified:   upload_docker.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -10,7 +10,7 @@ dockerpath=medhat16/udacity
 # Step 2
 # Run the Docker Hub container with kubernetes
 #kubectl run deployment boston-housing --image=$dockerpath --port=80
-kubectl run api-housing --image=$dockerpath --port=8080
+kubectl run udacity --image=$dockerpath --port=8080
 
 
 # Step 3:
@@ -22,4 +22,4 @@ kubectl get pods --all-namespaces
 # Forward the container port to a host
 #kubectl expose deployment api-housing --type=LoadBalancer --port=8000
 #kubectl port-forward deployment.apps/api-housing 8000:80
-kubectl expose deployment api-housing --type=LoadBalancer --port=8080:80
+kubectl expose deployment udacity --type=LoadBalancer --port=8080:80
